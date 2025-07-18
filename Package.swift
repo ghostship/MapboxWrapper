6e2eb555bafe9d643bc57da9a92d877d743594f1
Scott Keller <scott.keller@inspiringapps.com>
2025-07-18 08:59:35 -0600

    SPM

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "MapboxWrapper",
-            targets: ["MapboxwWrapper"]
+            targets: ["MapboxWrapper"]
         )
     ],
     dependencies: [
